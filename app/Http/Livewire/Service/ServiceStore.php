6ae305d7ebf64c726e1c774e9e33d078cfcdb2cf
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-09-06 16:12:09 -0300

    Service CRUD and views...

---
+++ app/Http/Livewire/Service/ServiceStore.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Service;
+
+use Livewire\Component;
+
+class ServiceStore extends Component
+{
+    public function render()
+    {
+        return view('livewire.service.service-store');
+    }
+}
