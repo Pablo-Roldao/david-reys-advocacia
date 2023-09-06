da4d5d24d36d86fb0f9c443d4a10e5d62ad544dc
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-09-06 13:09:40 -0300

    Team members CRUD and views...

---
+++ app/Http/Livewire/TeamMember/TeamMemberDestroy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\TeamMember;
+
+use Livewire\Component;
+
+class TeamMemberDestroy extends Component
+{
+    public function render()
+    {
+        return view('livewire.team-member.team-member-destroy');
+    }
+}
---
+++ app/Http/Livewire/TeamMember/TeamMemberShow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\TeamMember;
+
+use Livewire\Component;
+
+class TeamMemberShow extends Component
+{
+    public function render()
+    {
+        return view('livewire.team-member.team-member-show');
+    }
+}
