727bb047789a148b97824c14da4b37b4e8f2d068
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-09-11 09:17:43 -0300

    Image apresentation views...

---
+++ app/Models/Image/ImageApresentation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Image;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImageApresentation extends Model
+{
+    use HasFactory;
+}
