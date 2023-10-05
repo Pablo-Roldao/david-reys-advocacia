4f7618047b657374cd7a688855defcd3743f0099
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-10-05 17:48:41 -0300

    Useful links...

---
+++ app/Http/Livewire/UsefulLink/UsefulLinkEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\UsefulLink;
+
+use Livewire\Component;
+
+class UsefulLinkEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.useful-link.useful-link-edit');
+    }
+}
